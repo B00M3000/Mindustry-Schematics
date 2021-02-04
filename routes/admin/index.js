9acecaf6f88b52ac8c217ed9158816abd347f912
Thomas Zhou <doomking9000@gmail.com>
2021-02-04 03:43:12 +0000

    Update 1.5.2

---
+++ routes/admin/index.js
@@ -0,0 +1,28 @@
+const { Router } = require('express')
+var router = Router()
+
+const secret = "tadatada"
+
+router.use((req, res, next) => {
+  const { originalUrl } = req
+  
+  if(originalUrl == "/admin/" + secret){
+    res.cookie('secret', secret, { maxAge: 3600000}) //1 hour
+    return res.redirect('/admin')
+  }
+  
+  const s = req.cookies['secret']
+  
+  if(s && s == secret) return next()
+  
+  res.redirect('/')
+})
+
+router.get('/', (req, res) => {
+  res.render('admin')
+})
+
+router.use('/schematic_changes', require('./schematic_changes'))
+
+
+module.exports = router
---
+++ routes/admin/schematic_changes.js
@@ -0,0 +1,62 @@
+const { Router } = require('express')
+var router = Router()
+
+const schematicChangeSchema = require('../../schemas/SchematicChange.js')
+const schematicSchema = require('../../schemas/Schematic.js')
+
+router.param('_id', async (req, res, next, _id) => {
+  const change = await schematicChangeSchema.findOne({ _id })
+  
+  if(!change) return res.redirect('/admin/schematic_changes')
+  
+  req.change = change
+  
+  next()
+})
+
+router.get('/', async (req, res) => {
+  const changes = await schematicChangeSchema.find({})
+  res.render('schematic_changes', {
+    changes
+  })
+})
+
+router.get('/:_id', async (req, res) => {
+  const { change } = req
+  
+  res.render('schematic_change', {
+    change
+  })
+})
+
+router.get('/:_id/accept', async (req, res) => {
+  const { change } = req
+  
+  if(change.Delete){
+    await schematicSchema.deleteOne({
+      id: change.id
+    })
+  } else {
+    await schematicSchema.updateOne({
+      id: change.id
+    }, change.Changed)
+  }
+
+  await schematicChangeSchema.deleteOne({
+    _id: change._id
+  })
+  
+  res.redirect('/admin/schematic_changes')
+})
+
+router.get('/:_id/decline', async (req, res) => {
+  const { change } = req
+  
+  await schematicChangeSchema.deleteOne({
+    _id: change._id
+  })
+  
+  res.redirect('/admin/schematic_changes')
+})
+
+module.exports = router
