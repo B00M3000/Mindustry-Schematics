41feb9a05954d4d01d1aca07fb3f3d76a72bb384
Thomas Zhou <doomking9000@gmail.com>
2021-03-03 19:17:06 +0000

    Update 1.5.18

--- static/js/schematics.js
+++ static/js/schematics.js
@@ -1,45 +1,3 @@
-const sleep = async (delay) => await new Promise(resolve => setTimeout(resolve, delay));
-
-var copy_popup = document.getElementById('copy_popup')
-
-async function copy(text) {
-  var textArea = document.createElement("textarea");
-  textArea.value = text;
-  
-  // Avoid scrolling to bottom
-  textArea.style.top = "0";
-  textArea.style.left = "0";
-  textArea.style.position = "fixed";
-
-  document.body.appendChild(textArea);
-  
-  textArea.focus();
-  textArea.select();
-
-  var successful = document.execCommand('copy');
-
-  document.body.removeChild(textArea);
-
-  if(successful) {
-    var div = document.createElement('div')
-    div.id = "copied_popup"
-
-    var icon = document.createElement('img')
-    icon.src = "assets/paste.png"
-
-    var text = document.createElement('p')
-    text.innerText = "Copied to Clipboard!"
-
-    div.appendChild(icon)
-    div.appendChild(text)
-    document.body.appendChild(div)
-
-    await sleep(5000)
-
-    document.body.removeChild(div);
-  }
-}
-
 function keyUp() {
   var input = document.getElementById('schematics_search');
   var results = document.getElementById("schematics_result");
@@ -49,7 +7,6 @@ function keyUp() {
   var query = input.value.toUpperCase()
 
   for (var i = 0; i < names.length; i++) {
-    console.log(i, names)
     var nameElement = names[i].getElementsByTagName('h2')[0];
     var name = nameElement.innerText.toUpperCase();
     if (name.includes(query)) {
