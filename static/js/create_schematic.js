85a29901e54a4e46faced0bf8b1dcba0f9cec31a
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-03-16 19:02:34 -0300

    consume the api on frontend

--- static/js/create_schematic.js
+++ static/js/create_schematic.js
@@ -3,16 +3,43 @@ const name = document.getElementById('name')
 const description = document.getElementById('description')
 const image_preview = document.getElementById('image_preview')
 
-text.onchange = () => {
-  image_preview.src = `/api/schematics/image?text=${text.value}`
-  fetch("/api/schematics/parse?return=name%20description")
-    .then(function (myJson) {
-      const json = response.json();
+function isValidSchematic(base64Code) {
+  try {
+    const decoded = atob(base64Code);
+    const header = "msch";
+    // the startsWith method doesn't work
+    for (let i = 0; i < header.length; i++){
+      if (header[i] != decoded[i]) return false;
+    }
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+text && text.addEventListener('change', async () => {
+  const value = text.value
+  const form = document.querySelector('form')
+  form.classList.add('locked')
+  if (!isValidSchematic(value)) return
+  const response = await fetch(`/api/schematics/parse?text=${encodeURIComponent(value.trim())}`)
+  switch (response.status) {
+    case 200: {
+      const json = await response.json()
+      const imageData = json.image
       name.value = json.name
       description.value = json.description
-    })
-  
-}
+      let image = image_preview.querySelector("img")
+      if (!image) {
+        image = document.createElement('img')
+        image_preview.appendChild(image)
+      }
+      image.src = `data:image/png;base64,${imageData}`
+      form.classList.remove('locked')
+      }
+      break
+  }
+})
 
 fetch("https://ipinfo.io/json")
   .then(function (response) {
