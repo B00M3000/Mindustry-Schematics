593a38b1d069025653d7ed0c7b22a112935879da
Thomas Zhou <doomking9000@gmail.com>
2021-03-18 00:32:49 +0000

    Update tags_input.js

--- static/js/tags_input.js
+++ static/js/tags_input.js
@@ -5,35 +5,32 @@ if(tagsInputDiv){
   tagsInputDiv._list = tagsInputDiv.querySelector('ul');
   tagsInputDiv._input = tagsInputDiv.querySelector('input');
   tagsInputDiv._input._icategories = tagsInputDiv;
-  tagsInputDiv.onkeydown = function(e){
-    var e = event || e;
-    if(e.keyCode == 13) {
-      e.preventDefault();
-      
-      var c = e.target._icategories;
-      var value = c._input.value;
-      var tag = tags.find(t => t.name.toUpperCase() == value.toUpperCase())
-      if(tag && currentTags.indexOf(tag.name) === -1){
-        var li = document.createElement('li');
-        li.innerHTML = tag.name;
-        li.style.background = tag.color;
-        var x_mark = document.createElement('img');
-        x_mark.src = "/assets/x_mark.png"
-        x_mark.style.height = "10px"
-        x_mark.style.width = "13px"
-        li.appendChild(x_mark)
-        c._list.appendChild(li);
-        c._input.value = '';
+  tagsInputDiv._input.addEventListener('change', (event) => {
+    event.preventDefault();
+    
+    var c = event.target._icategories;
+    var value = c._input.value;
+    var tag = tags.find(t => t.name.toUpperCase() == value.toUpperCase())
+    if(tag && currentTags.indexOf(tag.name) === -1){
+      var li = document.createElement('li');
+      li.innerHTML = tag.name;
+      li.style.background = tag.color;
+      var x_mark = document.createElement('img');
+      x_mark.src = "/assets/x_mark.png"
+      x_mark.style.height = "10px"
+      x_mark.style.width = "13px"
+      li.appendChild(x_mark)
+      c._list.appendChild(li);
+      c._input.value = '';
 
-        currentTags.push(tag.name)
+      currentTags.push(tag.name)
 
-        x_mark.onclick = (e) => {
-          tagsInputDiv._list.removeChild(li)
-          currentTags.pop(currentTags.indexOf(tag.name))
-        }
-      } else {
-        c._input.value = '';
+      x_mark.onclick = (e) => {
+        tagsInputDiv._list.removeChild(li)
+        currentTags.pop(currentTags.indexOf(tag.name))
       }
+    } else {
+      c._input.value = '';
     }
-  }
+  })
 }
