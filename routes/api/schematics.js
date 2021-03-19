4d9c57e6159a747f6f61afc156b9b80783c1a564
Thomas Zhou <doomking9000@gmail.com>
2021-03-19 20:48:07 +0000

    deleted unused api route

--- routes/api/schematics.js
+++ routes/api/schematics.js
@@ -8,30 +8,6 @@ const schematicChangeSchema = require('../../schemas/SchematicChange.js')
 
 const limitPerPage = 20
 
-router.get('/', async (req, res) => {
-  var { query, page } = req.query
-  
-  var schematics;
-  
-  if(!page || isNaN(page) || page < 1 || page % 1 != 0) page = 1
-  else page = parseInt(page)
-  
-  const skip = limitPerPage * (page - 1);
-  
-  if(query){
-    const regex = new RegExp(query, "i")
-    const _query = { name: regex }
-    schematics = await schematicSchema.find(_query, null, { skip, limit: limitPerPage })
-    documents = await schematicSchema.countDocuments(_query)
-  } else {
-    query = ""
-    schematics = await schematicSchema.find(null, null, { skip, limit: limitPerPage })
-  }
-  res.send({
-    schematics
-  })
-})
-
 router.get('/parse', async (req, res) => {
   const { text } = req.query
   if (!text || text == '') {
