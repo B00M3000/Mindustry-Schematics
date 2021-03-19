c7910e569eb5b44531907fcc8ab47a8b2ae40dd8
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-03-19 16:52:08 -0300

    workaround for admin image problem

--- routes/raw/index.js
+++ routes/raw/index.js
@@ -2,16 +2,15 @@ const { Router } = require('express')
 var router = Router()
 
 const schematicChangeSchema = require('../../schemas/SchematicChange.js')
+const { Schematic } = require('mindustry-schematic-parser')
 
 router.use('/schematics', require('./schematics'))
 
 router.get('/schematic-changes/:_id/image', async (req, res) => {
   const change = await schematicChangeSchema.findOne({ _id: req.params._id })
-  
-  if(!change) return res.sendStatus(404)
-  
-  res.type('Content-Type', change.Changed.image.ContentType)
-  res.send(change.Changed.image.Data)
-})
+  if (!change) return res.sendStatus(404)
 
+  res.type('image/png')
+  res.send(await Schematic.decode(change.Changed.text).toImageBuffer())
+})
 module.exports = router
