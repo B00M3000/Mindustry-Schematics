8b1a22a7c5af8fd67615226f7b3ec755e41cdb74
Thomas Zhou <doomking9000@gmail.com>
2021-03-19 14:01:29 +0000

    Admin Panel works

--- routes/raw/index.js
+++ routes/raw/index.js
@@ -1,6 +1,17 @@
 const { Router } = require('express')
 var router = Router()
 
+const schematicChangeSchema = require('../../schemas/SchematicChange.js')
+
 router.use('/schematics', require('./schematics'))
+
+router.get('/schematic-changes/:_id/image', async (req, res) => {
+  const change = await schematicChangeSchema.findOne({ _id: req.params._id })
+  
+  if(!change) return res.sendStatus(404)
+  
+  res.type('Content-Type', change.Changed.image.ContentType)
+  res.send(change.Changed.image.Data)
+})
 
 module.exports = router
