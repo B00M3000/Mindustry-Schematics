40ab62cf4db10bded57be88e6b36172c9b58aace
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-03-28 11:12:21 -0300

    add routes

--- routes/next.ts
+++ routes/next.ts
@@ -5,5 +5,16 @@ const router = Router();
 router.get('/', (req, res) => {
   res.render('next/index.pug');
 });
-
+router.get('/downloads', (req, res) => {
+  res.render('next/downloads.pug');
+});
+router.get('/help', (req, res) => {
+  res.render('next/help.pug');
+});
+router.get('/info', (req, res) => {
+  res.render('next/info.pug');
+});
+router.get('/info/credits', (req, res) => {
+  res.render('next/credits.pug');
+});
 export default router;
