fe10d82f812c9723bf5509e87b1d40f5d1b3a9b8
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-04-17 12:35:56 -0300

    animate background transitions

--- static/js/dynamic_background.js
+++ static/js/dynamic_background.js
@@ -1,10 +1,15 @@
-const html = document.documentElement
+const html = document.documentElement;
 
-function updateBackground(){
-  const background = backgrounds[Math.floor((Math.random() * backgrounds.length))];
-  html.style.setProperty('--background', `url(${background})`)
+async function updateBackground() {
+  html.classList.add('active_background');
+  const current = html.style.getPropertyValue('--current-image');
+  if (current) html.style.setProperty('--previous-image', current);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const background =
+    backgrounds[Math.floor(Math.random() * backgrounds.length)];
+  html.style.setProperty('--current-image', `url(${background}`);
 }
 
-updateBackground()
+updateBackground();
 
-setInterval(updateBackground, 6000)
+setInterval(updateBackground, 6000);
