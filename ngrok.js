719dbd49cc4f86261dd68f266138c62083a53cd3
Thomas Zhou <>
2021-04-21 21:11:11 +0000

    Maybe working Ngrok Webhooks

--- ngrok.js
+++ ngrok.js
@@ -1,4 +1,11 @@
+'use strict';
 const ngrok = require('ngrok');
+const fetch = require('node-fetch')
+const path = require('path')
+
+require('dotenv').config({
+  path: path.join(__dirname, ".env")
+})
 
 ngrok.connect({
   authtoken: process.env.ngrok_token,
@@ -8,4 +15,20 @@ ngrok.connect({
 })
   .then(url => {
     console.log(`ngrok tunnel established, see output at ${url}`)
+    
+    fetch(process.env.WEBHOOK_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        embeds: [
+          {
+            title: "A ngrok tunnel has been established.",
+            description: "Tunnel Link: " + url,
+            url,
+          }  
+        ]
+      }
+    })
   })
