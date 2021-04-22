a7fadf5f4a48c652969e5744140f057aa6223b07
Thomas Zhou <>
2021-04-22 15:24:34 +0000

    Implemented Create for User tokens. (NOT WORKING) see client console when clicking save on anything

--- static/js/login.js
+++ static/js/login.js
@@ -14,6 +14,25 @@ function addSubmitListener(form){
   });
 }
 
+const template = document.getElementById('template')
+const users = document.querySelector('.users')
+function createToken(){
+  const form = template.cloneNode(true)
+  const token = generateId()
+  form.style.display = ""
+  form.id = `form-${token}`
+  form.action = `/user/${token}`
+  console.log(form)
+  form.querySelector('.delete').onclick = `deleteToken("${token}")`
+  form.querySelector('.regenerate').onclick = `regenerateToken("${token}")`
+  form.querySelector('input[name="token"]').id = token
+  form.querySelector('input[name="token"]').value = token
+  addSubmitListener(form)
+  const li = document.createElement('li')
+  li.append(form)
+  users.append(li)
+}
+
 async function deleteToken(token){
   const form = document.getElementById(`form-${token}`)
   if(form){
