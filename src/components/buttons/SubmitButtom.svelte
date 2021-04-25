334347e8c76c9acc9a745f4772434105923c68c5
Thomas Zhou <>
2021-04-25 20:10:35 +0000

    Maybe working SubmitButton Component

---
+++ src/components/buttons/SubmitButtom.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let value = "Submit";
+	
+  function handleClick(){
+    this.innerHTML = "Please wait..."
+  }
+</script>
+
+<button on:click={handleClick}> {value} </button>
+
+<style>
+	
+</style>
