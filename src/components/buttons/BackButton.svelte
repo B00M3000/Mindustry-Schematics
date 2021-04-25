8c5a1e18989f26ab967bba923ae3addba5e17497
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-04-24 23:31:48 -0300

    add "smart" feature for back button

--- src/components/buttons/BackButton.svelte
+++ src/components/buttons/BackButton.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
-  import IconButton from './IconButton.svelte';
+  import { navigation } from '@/src/stores';
 
+  import IconButton from './IconButton.svelte';
   export let href: string;
+  export let smart = false;
+  function goBack(e: MouseEvent) {
+    if (!smart) return;
+    const { previous } = $navigation;
+    if (previous && !previous.startsWith('http')) {
+      e.preventDefault();
+      return history.back();
+    }
+  }
 </script>
 
 <IconButton
@@ -10,7 +20,7 @@
   alt="back"
   border
   classname="invert-x"
-  on:click
+  on:click={goBack}
 >
   <span>Back</span>
 </IconButton>
