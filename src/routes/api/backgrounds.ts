d26c294e555a87ad048377e6dc927507d76be40e
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-05-03 09:48:24 -0300

    cache backgrounds

--- src/routes/api/backgrounds.ts
+++ src/routes/api/backgrounds.ts
@@ -1,10 +1,12 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import fs from 'fs';
 import path from 'path';
+let paths: string[] | undefined;
 export const get: RequestHandler = async () => {
-  const paths = fs
-    .readdirSync(path.resolve('static/assets/backgrounds'))
-    .map((file) => path.join('/assets/backgrounds', file).replace(/\\/g, '/'));
+  if (!paths)
+    paths = fs
+      .readdirSync(path.resolve('static/assets/backgrounds'))
+      .map((file) => path.join('/assets/backgrounds', file).replace(/\\/g, '/'));
   return {
     status: 200,
     body: paths,
