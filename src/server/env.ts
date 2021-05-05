87b74cf1a6398910a2e7f6b1a3afa15b13093402
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-05-05 08:56:43 -0300

    make env loading scalable

--- src/server/env.ts
+++ src/server/env.ts
@@ -1,21 +1,19 @@
 import dotenv from 'dotenv';
 import path from 'path';
 import fs from 'fs';
-
+/// preferred files to load environment variables, the first files on the array are the first to be used
+const envs = ['.env.dev', '.env'];
 function configEnv(): dotenv.DotenvParseOutput {
-  if (fs.existsSync(path.resolve('.env.dev'))) {
-    return (
-      dotenv.config({
-        path: path.resolve('.env.dev'),
-      }).parsed || {}
-    );
-  } else {
-    return (
-      dotenv.config({
-        path: path.resolve('.env'),
-      }).parsed || {}
-    );
+  for (const file of envs) {
+    const filePath = path.resolve(file);
+    if (fs.existsSync(filePath))
+      return (
+        dotenv.config({
+          path: filePath,
+        }).parsed || {}
+      );
   }
+  throw new Error('No .env files found');
 }
 
 const env = configEnv();
