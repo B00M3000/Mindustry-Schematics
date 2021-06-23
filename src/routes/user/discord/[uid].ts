5890c850d847e443ccf4bb11408c7601e326e429
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-06-23 08:52:37 -0300

    add todo

--- src/routes/user/discord/[uid].ts
+++ src/routes/user/discord/[uid].ts
@@ -1,6 +1,7 @@
 import { UserSchema } from '@/server/mongo';
 import type { RequestHandler } from '@sveltejs/kit';
-
+// TODO: make this more secure
+// this endpoint could be easily used to get the data of users without their consent
 export const get: RequestHandler = async (req) => {
   const uid = req.params.uid;
   const user = await UserSchema.findOne({ _id: uid });
