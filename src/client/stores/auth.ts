c77dac8ce5527079b94d721c774077df9b932fbe
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-06-23 20:08:52 -0300

    improve auth store

--- src/client/stores/auth.ts
+++ src/client/stores/auth.ts
@@ -2,28 +2,37 @@ import { session } from '$app/stores';
 import type { Session } from '@/interfaces/app';
 import { UserAccess } from '@/lib/auth/access';
 import { Writable, writable } from 'svelte/store';
-interface AuthStore {
+interface AuthState {
   name?: string;
   uid?: string;
   access: UserAccess;
 }
-const { set, subscribe } = writable<AuthStore>(
-  {
-    access: UserAccess.from(undefined),
-  },
-  (set) => {
-    const s = session as Writable<Session>;
-    s.subscribe(($session) => {
-      set({
-        name: $session.name,
-        uid: $session.uid,
-        access: UserAccess.from($session.access),
-      });
-    })();
-  },
-);
-export const auth = {
-  subscribe,
+class Auth {
+  private readonly store: Writable<AuthState>;
+  readonly subscribe: Writable<AuthState>['subscribe'];
+  constructor() {
+    this.store = writable(
+      {
+        access: UserAccess.from(undefined),
+      },
+      (set) => {
+        const s = session as Writable<Session>;
+        s.subscribe(($session) =>
+          set({
+            ...$session,
+            access: UserAccess.from($session.access),
+          }),
+        )();
+      },
+    );
+    this.subscribe = this.store.subscribe;
+  }
+
+  private set(value: AuthState) {
+    this.store.set(value);
+    session.set(value);
+  }
+
   async login(uid: string): Promise<void> {
     const response = await fetch('/api/user/login', {
       method: 'POST',
@@ -33,23 +42,24 @@ export const auth = {
     });
     if (response.status == 404) throw new Error('Uid does not exist');
     const data = await response.json();
-    set({
+    this.set({
       ...data,
       uid,
       access: UserAccess.from(data.access),
     });
-  },
+  }
   async logout(): Promise<void> {
     await fetch('/api/user/logout', {
       method: 'POST',
     });
-    set({
+    this.set({
       access: UserAccess.from(undefined),
     });
-  },
+  }
   async sync(): Promise<void> {
     let uid: string | undefined;
     this.subscribe(($auth) => (uid = $auth.uid))();
     if (uid) await this.login(uid);
-  },
-};
+  }
+}
+export const auth = new Auth();
