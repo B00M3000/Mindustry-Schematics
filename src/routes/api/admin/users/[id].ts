6f5d74c735129930ab02cd51632885420631e4a4
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-06-30 18:38:00 -0300

    add api to modify users

---
+++ src/routes/api/admin/users/[id].ts
@@ -0,0 +1,57 @@
+import type { Context } from '@/interfaces/app';
+import { UserAccess } from '@/lib/auth/access';
+import { UserSchema } from '@/server/mongo';
+import { parseForm } from '@/server/parse_body';
+import type { RequestHandler } from '@sveltejs/kit';
+interface Params {
+  verified: boolean;
+  access: string;
+}
+export const post: RequestHandler<Context> = async ({ context, body, params }) => {
+  const { verified, access } = parseForm<Params>(body);
+  const userAccess = UserAccess.from(context.access);
+  if (!userAccess.can({ users: { read: 'all', update: 'all' } }))
+    return {
+      status: 403,
+      body: {
+        message: 'Forbidden access',
+      },
+    };
+  if (!(verified && access))
+    return {
+      status: 400,
+      body: {
+        message: 'Missing required fields',
+      },
+    };
+  if (context.uid == params.id)
+    return {
+      status: 401,
+      body: {
+        message: "A user can't modify its own privileges",
+      },
+    };
+  const newAccess = UserAccess.from(access);
+  if (newAccess.compare(userAccess) > 0)
+    return {
+      status: 401,
+      body: {
+        message: "You can't promove a user to a level higher than yours",
+      },
+    };
+  await UserSchema.findOneAndUpdate(
+    {
+      id: params.id,
+    },
+    {
+      verified,
+      access,
+    },
+  );
+  return {
+    status: 200,
+    body: {
+      message: 'ok',
+    },
+  };
+};
