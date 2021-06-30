a8ff6716dc19d40ef9503a76b0b8f4ef976bb69c
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-06-30 18:37:51 -0300

    add api to get users

---
+++ src/routes/api/admin/users/index.ts
@@ -0,0 +1,43 @@
+import type { Context } from '@/interfaces/app';
+import type { UserSearchJSON } from '@/interfaces/json';
+import { UserAccess } from '@/lib/auth/access';
+import { escapeRegexString } from '@/lib/string';
+import { UserDocument, UserSchema } from '@/server/mongo';
+import type { RequestHandler } from '@sveltejs/kit';
+import type { FilterQuery } from 'mongoose';
+export const get: RequestHandler<Context> = async ({ context, query }) => {
+  const access = UserAccess.from(context.access);
+  if (!access.can({ users: { read: 'all' } }))
+    return {
+      status: 403,
+      body: {
+        message: 'Forbidden',
+      },
+    };
+  const limit = 50;
+  const search = query.get('search');
+  const filter: FilterQuery<UserDocument> = {};
+  if (search) filter.tag = new RegExp(escapeRegexString(search));
+  const documents = await UserSchema.countDocuments(filter);
+  const pages = Math.ceil(documents / limit) || 1;
+  let page = Number(query.get('page') || 1);
+  if (page < 1) page = 1;
+  if (page > pages) page = pages;
+  const users = await UserSchema.find(filter, 'id tag access verified', {
+    limit,
+    skip: (page - 1) * limit,
+    sort: {
+      tag: -1,
+    },
+  });
+  const result: UserSearchJSON = {
+    page,
+    users,
+    pages,
+    total: documents,
+  };
+  return {
+    status: 200,
+    body: result,
+  };
+};
