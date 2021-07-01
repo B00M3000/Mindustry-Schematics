4b99ec23eb4a39fc0915740a403acec1e41d219b
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-07-01 12:29:12 -0300

    add basic user query

--- src/routes/admin/users/index.svelte
+++ src/routes/admin/users/index.svelte
@@ -17,6 +17,7 @@
     return {
       props: {
         data: json,
+        query: page.query,
       },
     };
   };
@@ -25,27 +26,54 @@
 <script lang="ts">
   import BackButton from '@/client/components/buttons/BackButton.svelte';
   import Card from './_card.svelte';
-
+  import { goto } from '$app/navigation';
   export let data: UserSearchJSON;
+  export let query: URLSearchParams;
+  async function search(e: Event) {
+    e.preventDefault();
+    let form = e.currentTarget as HTMLFormElement;
+    let formData = new FormData(form);
+    let tag = formData.get('tag');
+    let url = '/admin/users';
+    if (tag) {
+      url += `?tag=${tag}`;
+    }
+    await goto(url);
+  }
 </script>
 
 <template lang="pug">
-  form.search
+  form.search(on:submit!="{search}")
     label(for="tag") Tag: 
-    input#tag(type="text")
+    input#tag(type="text" name="tag" value!="{query.get('tag')}")
+    button Search
+
   ul.result
     +each("data.users as user")
       Card({user})    
-
+      +else
+        span No results
   footer
     BackButton(href="/user" smart)
 </template>
 
 <style>
+  form.search {
+    margin: 1em 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1em;
+    padding: 0 1rem;
+    z-index: 10;
+    flex-wrap: wrap;
+  }
   ul.result {
     display: flex;
-    flex-direction: column;
+    flex-wrap: wrap;
     align-items: center;
+    justify-content: center;
     gap: 1.5em;
+    padding: 0 1rem;
   }
 </style>
