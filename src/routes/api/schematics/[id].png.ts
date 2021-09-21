b48a6fa86b74edb9a687ae48bd2808a99483f44a
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-09-21 09:35:30 -0300

    fix image filename error

--- src/routes/api/schematics/[id].png.ts
+++ src/routes/api/schematics/[id].png.ts
@@ -5,12 +5,13 @@ export const get: RequestHandler = async (req) => {
   const schematic = await SchematicSchema.findOne({ _id: req.params.id });
 
   if (!schematic) return { status: 404, body: 'Not found' };
-  const body = Buffer.from(schematic.image.Data.buffer);
+  const body = schematic.image.Data;
+  const filename = encodeURIComponent(schematic.name);
   return {
     status: 200,
     headers: {
       'cache-control': 'max-age=1800',
-      'content-disposition': `inline; filename="${schematic.name}.png"`,
+      'content-disposition': `inline; filename="${filename}.png"`,
       'content-type': 'image/png',
       'content-length': body.length.toString(),
     },
