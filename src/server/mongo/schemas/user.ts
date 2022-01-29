1ebefd573ed4b89ec38bbdaedba61a06224b4485
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2022-01-28 22:12:10 -0500

    Update user.ts

--- src/server/mongo/schemas/user.ts
+++ src/server/mongo/schemas/user.ts
@@ -2,23 +2,11 @@ import mongoose from 'mongoose';
 
 const schema = new mongoose.Schema({
   id: { type: String, required: true },
-  username: { type: String, required: true },
-  discriminator: { type: String, required: true },
-  avatar_hash: { type: String },
-  avatar_url: { type: String },
-  tag: { type: String, required: true },
-  verified: { type: Boolean },
-  access: { type: String, required: true, default: 'none' },
+  discord_id: String
 });
 export interface UserDocument extends mongoose.Document {
   id: string;
-  username: string;
-  discriminator: string;
-  tag: string;
-  avatar_hash?: string;
-  avatar_url?: string;
-  verified?: boolean;
-  access: string;
+  discord_id?: string;
 }
 
 export const UserSchema: mongoose.Model<UserDocument> =
