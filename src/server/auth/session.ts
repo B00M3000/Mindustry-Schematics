477fab061ad7ad3fe39c835e03ec264fd7d9ff17
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-01-30 16:06:27 -0300

    fix session user_id intialization

--- src/server/auth/session.ts
+++ src/server/auth/session.ts
@@ -1,5 +1,4 @@
 import { SessionSchema } from '@/server/mongo';
-import { UserAccess, accessLevels } from '@/lib/auth/access';
 
 interface SessionOptions {
   session_id: string;
@@ -8,16 +7,16 @@ interface SessionOptions {
 
 export class ServerSession {
   session_id: string;
-  user_id: string;
+  user_id?: string;
 
   constructor(options: SessionOptions) {
-    ({ session_id: this.session_id } = options);
+    ({ session_id: this.session_id, user_id: this.user_id } = options);
   }
 
   static async get(session_id?: string): Promise<ServerSession | undefined> {
     if (!session_id) return;
     const sessionDoc = await SessionSchema.findOne({
-      _id: session_id.session_id,
+      _id: session_id,
     });
     if (!sessionDoc) return;
     const session = new ServerSession({
