22766c41ef83f631dda1b92ecfbf9121185b3b65
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-01-30 19:21:57 -0300

    fix avatar endpoint

--- src/routes/user/avatar.json.ts
+++ src/routes/user/avatar.json.ts
@@ -5,14 +5,13 @@ import { parseForm } from '@/server/parse_body';
 interface PostBody {
   content_type: string;
   data: string;
+  id: string;
 }
 
 export const post: RequestHandler = async (req) => {
   const parsedForm = parseForm<PostBody>(req.body);
   const { content_type, data, id } = parsedForm;
   //const { id } = req.locals;
-
-  avatar_url: 
 
   if (!id)
     return {
@@ -21,28 +20,32 @@ export const post: RequestHandler = async (req) => {
       body: { message: 'User is not Authenticated' },
     };
 
-  if (!content_type || !data){
-    const user = await UserSchema.findOne({ _id: id })
-    const discord_user = await DiscordSchema.findOne({ id: user.discord_id })
-    
-    const avatar_url = "https://cdn.discordapp.com/avatars/" + ( discord_user.avatar_hash
-        ? `${discord_user.id}/${discord_user.avatar_hash}.png`
-        : `${getRandomInt(5)}.png` )
+  if (!content_type || !data) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const user = (await UserSchema.findOne({ _id: id }))!;
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const discord_user = (await DiscordSchema.findOne({ id: user!.discord_id }))!;
 
-    console.log(avatar_url)
+    const avatar_url =
+      'https://cdn.discordapp.com/avatars/' +
+      (discord_user.avatar_hash
+        ? `${discord_user.id}/${discord_user.avatar_hash}.png`
+        : `${getRandomInt(5)}.png`);
+
+    console.log(avatar_url);
 
     const response = await fetch(avatar_url, {
       headers: {
-        'Content-Type': "image/png; charset=UTF-8"
-      }
-    })
+        'Content-Type': 'image/png; charset=UTF-8',
+      },
+    });
 
-    const imageBlob = await response.buffer()
+    const imageBuffer = await response.arrayBuffer();
 
     await UserSchema.findOneAndUpdate(
       { _id: id },
       {
-        avatar: imageToDataURL("image/png", "image")
+        avatar: imageToDataURL('image/png', imageBuffer),
       },
     );
 
@@ -56,7 +59,7 @@ export const post: RequestHandler = async (req) => {
   await UserSchema.findOneAndUpdate(
     { _id: id },
     {
-      avatar: imageToDataURL(content_type, data)
+      avatar: imageToDataURL(content_type, data),
     },
   );
 
@@ -67,11 +70,11 @@ export const post: RequestHandler = async (req) => {
   };
 };
 
-function getRandomInt(max) {
+function getRandomInt(max: number) {
   return Math.floor(Math.random() * max);
 }
 
-function imageToDataURL(content_type, data) {
-  const base64 = Buffer.from(data, 'base64');
-  return `data:${content_type};base64,${base64}`
+function imageToDataURL(content_type: string, data: ArrayBuffer | string) {
+  const base64 = typeof data === 'string' ? data : Buffer.from(data).toString('base64');
+  return `data:${content_type};base64,${base64}`;
 }
