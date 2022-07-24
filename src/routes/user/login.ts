5600354fa63a2f672b2453fcfdaebca4e9144771
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2022-07-24 18:28:38 -0400

    Update login.ts

--- src/routes/user/login.ts
+++ src/routes/user/login.ts
@@ -8,7 +8,7 @@ export const GET: RequestHandler = async () => {
     response_type: 'code',
     prompt: 'none',
   });
-  console.log("Appid: " + params.getAll()) // debug
+  console.log("Appid: " + params.get('client_id')) // debug
   return {
     status: 308,
     headers: {
