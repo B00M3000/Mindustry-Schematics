967289bd4aaf49646622fc3308b74d198934f3be
JeanJPNM <jeanjpnm1@gmail.com>
2022-07-24 17:58:50 -0300

    update avatar endpoint

--- src/routes/user/avatar_url.ts
+++ src/routes/user/avatar_url.ts
@@ -1,18 +1,19 @@
 import type { RequestHandler } from '@sveltejs/kit';
-import { UserSchema } from '@/server/mongo'
+import { UserSchema } from '@/server/mongo';
 import * as cookie from 'cookie';
 
-export const get: RequestHandler<never> = async (req) => {
-    const user_id = req.url.searchParams.get('user')
-    
-    if(user_id){
-        const user = await UserSchema.findOne({ _id: user_id })
-        if(user){
-            return { body: user.avatar_url };
-        } else {
-            return { status: 400, body: "Invalid User Id" };
-        }
-    } else {
-        return { body: req.locals.user.avatar_url };
+export const get: RequestHandler<never> = async ({ locals, url }) => {
+  const user_id = url.searchParams.get('user');
+
+  if (user_id) {
+    const user = await UserSchema.findOne({ _id: user_id });
+    if (user) {
+      return { body: user.avatar_url };
     }
+    return { status: 400, body: 'Invalid User Id' };
+  }
+
+  if (locals.user) return { body: locals.user?.avatar_url };
+
+  return { status: 404, body: 'Missing user id' };
 };
