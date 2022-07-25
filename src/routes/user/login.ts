7595650079b1e9574527035f4c8128e8d502a08a
JeanJPNM <jeanjpnm1@gmail.com>
2022-07-25 13:56:48 -0300

    fix redirect_url

--- src/routes/user/login.ts
+++ src/routes/user/login.ts
@@ -1,22 +1,24 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import env from '@/server/env';
 export const GET: RequestHandler = async () => {
+  const redirect_url = new URL('/user/redirect', env.WEBSITE_URL);
+
   const params = new URLSearchParams({
     client_id: env.DISCORD_APPLICATION_ID!,
     scope: 'identify guilds.join',
-    redirect_uri: `${env.WEBSITE_URL}/user/redirect`,
+    redirect_uri: redirect_url.toString(),
     response_type: 'code',
     //prompt: 'none',
   });
-  
+
   return {
-    status: 308,
+    status: 307,
     headers: {
       location: `https://discord.com/api/oauth2/authorize?${params}`,
     },
     body: {
       message: 'Redirect to Discord Oauth',
-      location: `https://discord.com/api/oauth2/authorize?${params}`
+      location: `https://discord.com/api/oauth2/authorize?${params}`,
     },
   };
 };
