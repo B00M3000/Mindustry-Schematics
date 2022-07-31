c1a1ab6689598afbc8081567806bb29e261f92a6
JeanJPNM <jeanjpnm1@gmail.com>
2022-07-31 12:31:34 -0300

    extract items into a reusable array

---
+++ src/lib/items.ts
@@ -0,0 +1,26 @@
+import type { ItemName } from 'mindustry-schematic-parser';
+
+export const items: ItemName[] = [
+  'beryllium',
+  'blast-compound',
+  'carbide',
+  'coal',
+  'copper',
+  'dormant-cyst',
+  'fissile-matter',
+  'graphite',
+  'lead',
+  'metaglass',
+  'metaglass',
+  'oxide',
+  'phase-fabric',
+  'plastanium',
+  'pyratite',
+  'sand',
+  'scrap',
+  'spore-pod',
+  'surge-alloy',
+  'thorium',
+  'titanium',
+  'tungsten',
+];
