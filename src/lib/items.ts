624206f38339b5e8576866708e7df4b5fe9cd361
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-07-31 12:39:05 -0300

    Add v7 items and liquids (#164)
    * add icons for the new items and liquids
    
    * optimize item svgs
    
    * optimize liquid svgs
    
    * extract items into a reusable array
    
    * sort the item array

---
+++ src/lib/items.ts
@@ -0,0 +1,29 @@
+import type { ItemName } from 'mindustry-schematic-parser';
+
+/**
+ * A constant array containing the names of all the mindustry items, it is sorted
+ * by their declaration order
+ */
+export const items: ItemName[] = [
+  'copper',
+  'lead',
+  'metaglass',
+  'graphite',
+  'sand',
+  'coal',
+  'titanium',
+  'thorium',
+  'scrap',
+  'silicon',
+  'plastanium',
+  'phase-fabric',
+  'surge-alloy',
+  'blast-compound',
+  'pyratite',
+  'beryllium',
+  'tungsten',
+  'oxide',
+  'carbide',
+  'fissile-matter',
+  'dormant-cyst',
+];
