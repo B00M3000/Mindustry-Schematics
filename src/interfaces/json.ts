c22a6f54921a84c2532d69cea7eec0952ed578fb
JeanJPNM <jeanjpnm1@gmail.com>
2022-07-30 22:17:18 -0300

    remove unusde "mode" field from SchematicQueryJSON

--- src/interfaces/json.ts
+++ src/interfaces/json.ts
@@ -17,7 +17,6 @@ export interface SchematicQueryJSON {
   documents: number;
   schematics: BasicSchematicJSON[];
   tags: string;
-  mode: 'creator' | 'name';
 }
 
 export interface SchematicParseJSON {
