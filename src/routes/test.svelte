1bb09e6529fecf0453ecb02d32160947506bd8e9
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2022-08-01 16:23:37 -0400

    nothing to see here

--- src/routes/test.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import AuthorCard from '@/client/components/AuthorCard.svelte';
-</script>
-
-<template lang="pug">
-    AuthorCard(creator_id="62ded6506a6a5f4783135f70")
-</template>
