ac3a6da77725de46f9814826822f3e090d07d0bb
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2022-08-22 07:26:01 -0500

    prevent non hex string session_id from causing errors

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,10 +1,12 @@
 import cookie from 'cookie';
 import type { GetSession, Handle } from '@sveltejs/kit';
+import mongoose  from 'mongoose'
 import mongo from '@/server/mongo';
 import { SessionSchema, UserSchema } from './server/mongo';
 import webhooks from './server/webhooks';
 import { dev } from '$app/env';
 import { UserAccess } from './lib/auth/access';
+import { session } from '$app/stores';
 
 const dbPromise = mongo();
 
@@ -23,7 +25,7 @@ export const handle: Handle = async ({ event, resolve }) => {
     await dbPromise;
     const cookies = cookie.parse(event.request.headers.get('cookie') || '');
     const session_id = cookies.session_id;
-    if (session_id) {
+    if (session_id && mongoose.isObjectIdOrHexString(session_id)) {
       const session = await SessionSchema.findOne({ _id: session_id });
       if (session) {
         const user = await UserSchema.findOne({ _id: session.user_id });
@@ -37,8 +39,10 @@ export const handle: Handle = async ({ event, resolve }) => {
           };
         }
       } else {
-        //TBA: clear session cookie
+        //event.request.headers.set('set-cookie', cookie.serialize('session_id', ''))
       }
+    } else {
+      //event.request.headers.set('set-cookie', cookie.serialize('session_id', ''))
     }
 
     const response = await resolve(event);
