318b9a2ad3e49abd6da856b9f03f5a08b911e3a4
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2022-11-13 23:08:55 -0500

    d

--- svelte.config.js
+++ svelte.config.js
@@ -11,11 +11,6 @@ const config = {
     adapter: adapter({
       out: 'build',
     }),
-
-    // Override http methods in the Todo forms
-    methodOverride: {
-      allowed: ['PATCH', 'DELETE'],
-    },
   },
 };
 
