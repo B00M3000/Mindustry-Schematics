ca36f530656d61e45d2ce9dbaa7bd14884489cf4
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-01-28 16:35:09 -0500

    Updated BasicSchematicJSON to include votes

--- src/interfaces/json.ts
+++ src/interfaces/json.ts
@@ -7,7 +7,7 @@ import type {
 export type SchematicJSON = Omit<LeanDocument<SchematicDocument>, 'image'>;
 export type BasicSchematicJSON = Pick<
   LeanDocument<SchematicDocument>,
-  '_id' | 'creator_id' | 'name' | 'text'
+  '_id' | 'creator_id' | 'name' | 'text' | 'votes'
 >;
 export interface SchematicQueryJSON {
   skip: number;
