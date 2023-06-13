ecea35c0f0e69a8bf26d53a2adbce9298bd35391
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-06-13 18:27:02 -0400

    Added vote processing endpoint

---
+++ src/routes/schematics/[id=dbid]/vote/+server.ts
@@ -0,0 +1,26 @@
+import { error, json } from '@sveltejs/kit';
+import type { SchematicDocument } from '@/server/mongo';
+import { SchematicSchema } from '@/server/mongo';
+import type { RequestHandler } from './$types';
+import { match } from '@/params/dbid';
+
+export const GET: RequestHandler = async ({ request, params, url,  locals }) => {   
+  if (!locals.user) throw error(403, 'Unauthorized, please login before trying again.');
+
+  switch (url.searchParams.get('v')) {
+    case undefined:
+      return json({ body: { message: 'No vote choice recieved. (Search param v is undefined)' }}, { status: 400 });
+      break;
+    case 'up':
+      await SchematicSchema.updateOne({ _id: params.id }, { votes: { $set: { [locals.user.id]: 1 }}});
+      break;
+    case 'down':
+      await SchematicSchema.updateOne({ _id: params.id }, { votes: { $set: { [locals.user.id]: -1 }}});
+      break;
+    case 'none':
+      await SchematicSchema.updateOne({ _id: params.id }, { votes: { $unset: { [locals.user.id]: 0 }}});
+      break;
+  }
+
+  return json({ body: { message: "Your vote was processed sucessfully!"}}, { status: 200 })
+};
