c649fd52a0b091dfb8ed6b89e3f652dca4cd7494
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-06-13 00:31:54 -0400

    Reworked MongoDB Environment Variables
    Removed username, password, database, and path configuration in light of just using a preformatted uri environment variable.

--- src/server/mongo/index.ts
+++ src/server/mongo/index.ts
@@ -1,22 +1,15 @@
 import env from '@/server/env';
 import mongoose from 'mongoose';
 
-const username = env.MONGO_USER || '';
-const password = env.MONGO_PASS || '';
-const database = env.MONGO_DATABASE_NAME || 'Schematics';
-
-if (!env.MONGO_PATH) throw new Error('Mongo db path is not defined');
-
-const path = env.MONGO_PATH.replace('<username>', username)
-  .replace('<password>', password)
-  .replace('<database>', database);
+const uri = env.MONGO_URI
+if (!uri) throw new Error('MongoDB uri is not defined in env.');
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async () => {
   if (mongoose.connections.length > 0) {
     await mongoose.disconnect();
   }
-  await mongoose.connect(path);
+  await mongoose.connect(uri);
 };
 
 export * from './schemas/schematic';
