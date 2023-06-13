68cbb525bbf9bc45a339a679de5ee3f76832af14
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2023-06-13 07:36:21 -0300

    load the whole background before the transition (#168)
    This makes background transitions smoother on lower internet
    speeds, although it also makes them lag behind the actual
    navigation event, this problem can be amortized by caching the images

--- src/routes/_background.svelte
+++ src/routes/_background.svelte
@@ -3,7 +3,9 @@
   import { onMount } from 'svelte';
 
   export let backgrounds: string[];
+  let backgroundCache = new Map<string, string>();
   let html: HTMLElement;
+  let current: string | undefined;
   let busy = false;
 
   afterNavigate(() => {
@@ -18,25 +20,31 @@
   const delay = (milliseconds: number) =>
     new Promise<void>((resolve) => setTimeout(resolve, milliseconds));
 
-  function nextBackground(current: string) {
-    let background = backgrounds[Math.floor(Math.random() * backgrounds.length)];
-    while (current == `url("${background}"")`) {
+  function nextBackground() {
+    let background: string;
+    do {
       background = backgrounds[Math.floor(Math.random() * backgrounds.length)];
-    }
+    } while (current == background);
+
     return background;
   }
   async function updateBackground() {
     busy = true;
 
-    let previous = html.style.getPropertyValue('--solid-image');
-    let next = nextBackground(previous);
+    let next = nextBackground();
 
-    html.style.setProperty('--temp-image', `url("${next}")`);
+    let base64Data = backgroundCache.get(next);
+    if (!base64Data) {
+      base64Data = await fetchImage(next);
+      backgroundCache.set(next, base64Data);
+    }
+
+    html.style.setProperty('--temp-image', `url("${base64Data}")`);
     html.style.setProperty('--temp-opacity', '1');
 
     await delay(1000);
 
-    html.style.setProperty('--solid-image', `url("${next}")`);
+    html.style.setProperty('--solid-image', `url("${base64Data}")`);
 
     await delay(200);
 
@@ -44,6 +52,19 @@
 
     await delay(600);
 
+    current = next;
     busy = false;
+  }
+
+  async function fetchImage(url: string) {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const reader = new FileReader();
+
+    return await new Promise<string>((resolve, reject) => {
+      reader.addEventListener('error', reject);
+      reader.addEventListener('load', (e) => resolve(reader.result as string));
+      reader.readAsDataURL(blob);
+    });
   }
 </script>
