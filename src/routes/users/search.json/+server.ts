3451e37414528dd4d5c636576e6a7cf5bc68382a
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-06-14 15:27:33 -0400

    Added User Search Functionality

---
+++ src/routes/users/search.json/+server.ts
@@ -0,0 +1,39 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { UserSchema, type UserDocument } from '@/server/mongo';
+import type { FilterQuery } from 'mongoose';
+import { getPaginatedQueryPosition } from '@/server/schematic_pagination';
+
+const limitPerPage = 10;
+
+export const GET: RequestHandler = async ({ url }) => {
+  const query = url.searchParams.get('q') || '';
+
+  const dbQuery: FilterQuery<UserDocument> = {};
+  if (query) {
+    dbQuery.username = new RegExp(query.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'), 'i');
+  }
+  try {
+    const documents = await UserSchema.countDocuments(dbQuery);
+
+    const users = await UserSchema.find(dbQuery, '_id', {
+      limit: limitPerPage,
+      sort: {
+        _id: -1,
+      },
+    });
+
+    const body = {
+      users,
+      query
+    };
+    
+    return json(body, {
+      headers: {
+        'cache-control': 'max-age=120',
+      },
+    });
+  } catch (e) {
+    return json({ error: String(e) }, { status: 500 });
+  }
+};
