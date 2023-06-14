1ea3b5b6d0264512cae6ef17115671b0e342d1c5
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-06-14 16:50:57 -0400

    Creates a new vote array if perviously undefined

--- src/routes/schematics/[id=dbid]/vote/+server.ts
+++ src/routes/schematics/[id=dbid]/vote/+server.ts
@@ -2,11 +2,13 @@ import { error, json } from '@sveltejs/kit';
 import type { SchematicDocument } from '@/server/mongo';
 import { SchematicSchema } from '@/server/mongo';
 import type { RequestHandler } from './$types';
-import { match } from '@/params/dbid';
 
 export const GET: RequestHandler = async ({ request, params, url,  locals }) => {   
   if (!locals.user) throw error(403, 'Unauthorized, please login before trying again.');
-
+  
+  const isUndefined = (await SchematicSchema.findOne({ _id: params.id }, 'votes'))?.votes == undefined;
+  if(isUndefined) await SchematicSchema.updateOne({ _id: params.id }, { votes: { 'hi': "bye" } })
+  
   switch (url.searchParams.get('v')) {
     case undefined:
       return json({ body: { message: 'No vote choice recieved. (Search param v is undefined)' }}, { status: 400 });
@@ -15,12 +17,14 @@ export const GET: RequestHandler = async ({ request, params, url,  locals }) => 
       await SchematicSchema.updateOne({ _id: params.id }, { $set: { votes: { [locals.user.id]: 1 }}});
       break;
     case 'down':
-      await SchematicSchema.updateOne({ _id: params.id }, { $set:  { votes: { [locals.user.id]: -1 }}});
+      await SchematicSchema.updateOne({ _id: params.id }, { $set: { votes: { [locals.user.id]: -1 }}});
       break;
     case 'none':
-      await SchematicSchema.updateOne({ _id: params.id }, { $unset: { votes: { [locals.user.id]: 0 }}});
+      await SchematicSchema.updateOne({ _id: params.id  }, { $unset: { votes: { [locals.user.id]: 0 }}});
       break;
+    default:
+      return json({ body: { message: `Invalid vote choice recived.`}}, { status: 400 })
   }
 
-  return json({ body: { message: "Your vote was processed sucessfully!"}}, { status: 200 })
+  return json({ body: { message: `Your ${url.searchParams.get('v')} vote was processed sucessfully!`}}, { status: 200 })
 };
